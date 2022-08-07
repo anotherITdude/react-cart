c0c67b926f757a67d70474843c05330d69282fe3
Anotheritdude <abhi.baidya@gmail.com>
2022-08-07 12:29:49 +0200

    initial setup nextjs/tailwind/typescript

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className=''>
+     
+      <h1 className=' text-2xl mt-11 mb-11 text-center'>
+        ReactJS/Tailwind Shopping cart using nextJS
+      </h1>
+      <footer className={styles.footer}>
+        
+      </footer>
+    </div>
+  )
+}
