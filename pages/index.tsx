463e254d0dcb4d25263c82b1cde889f8d1fb118b
Anotheritdude <abhi.baidya@gmail.com>
2022-08-08 15:25:19 +0200

    public folder 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import Header from '../components/header/Header';
 import Head from 'next/head'
-import Logo from '../public/favicon.png';
 
 const Home = (): JSX.Element => {
   return (
     <div className=''>
       <Head >
-      <title>React Store</title>
+      <title>Anotheritdude | React Store</title>
+      <link rel="icon" type="image/x-icon" href='https://ibb.co/SDSkKtt' />
       </Head>
       <Header />
       <h1 className=' text-rs-blue text-2xl mt-11 mb-11 text-center'>
